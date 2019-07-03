92cf6bc891f5a4cfca63de66a08f05b0630b9be9
BigBin <540305341@qq.com>
2019-07-03 21:29:21 +0800

    git status

--- C/1-TwoSum.c
+++ C/1-TwoSum.c
@@ -1,6 +1,13 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+/**
+ * 最普通的暴力写法
+ * 时间复杂度 O(n*n)
+ * 空间复杂度O(1)
+ */
+#if 0
 int* twoSum(int* nums, int numsSize, int target) {
     static int targetArr[2] = {0};
     for(int index = 0 ; index < numsSize -1 ; index ++)
@@ -16,4 +23,48 @@ int* twoSum(int* nums, int numsSize, int target) {
         }      
     }
     return targetArr;
+}
+#endif 
+
+/**
+ * 一次hash的方法
+ */
+# include <string.h>
+
+typedef struct Node
+{
+    int key;
+    int value;
+    struct Node *next;
+}node;
+
+typedef struct HashTab
+{
+    int keyNum;
+    struct Node *head;
+}hashTab;
+
+int numTo2N(int num)
+{
+    int i = 1 , tmp = num;
+    while(num >>= 1){
+        i <<= 1;
+    }
+    
+    return (i<tmp) ? i<<1 : i;
+}
+
+int* twoSum(int* nums, int numsSize, int target) {
+    static int targetArr[2] = {0};
+    int index = 0;
+    int hashLength = numTo2N(numsSize);
+    hashTab * pHashTab = (hashTab * )malloc(sizeof(hashTab) * hashLength);
+
+    if(pHashTab = 0)
+    {
+        return targetArr;
+    }
+
+    memset(pHashTab , 0 , sizeof(hashTab) * hashLength);
+    
 }
